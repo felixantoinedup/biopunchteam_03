6876870c4987fc2c09bd1b400837f7c6caf1f732
antronik7 <antoineberthiaumedutil@gmail.com>
2019-01-20 00:58:36 -0500

    Basic Unreal Project

---
+++ BiopunchJam3/Intermediate/Build/Win64/UE4Editor/Inc/BiopunchJam3/MyActor.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BIOPUNCHJAM3_MyActor_generated_h
+#error "MyActor.generated.h already included, missing '#pragma once' in MyActor.h"
+#endif
+#define BIOPUNCHJAM3_MyActor_generated_h
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_RPC_WRAPPERS
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend BIOPUNCHJAM3_API class UClass* Z_Construct_UClass_AMyActor(); \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BiopunchJam3"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend BIOPUNCHJAM3_API class UClass* Z_Construct_UClass_AMyActor(); \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BiopunchJam3"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public:
+
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMyActor)
+
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_9_PROLOG
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_RPC_WRAPPERS \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_INCLASS \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+	BiopunchJam3_Source_BiopunchJam3_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BiopunchJam3_Source_BiopunchJam3_MyActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
