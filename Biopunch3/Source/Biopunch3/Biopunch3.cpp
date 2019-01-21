d4ba7b9a49fb91573ff7c317d953d388acb2b278
antronik7 <antoineberthiaumedutil@gmail.com>
2019-01-20 19:20:59 -0500

    Changing Version

--- Biopunch3/Source/Biopunch3/Biopunch3.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "Biopunch3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Biopunch3, "Biopunch3" );
- 
