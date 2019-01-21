5d4484e7bf7679a66bae6397ac2af27af99ecf1f
antronik7 <antoineberthiaumedutil@gmail.com>
2019-01-21 00:19:48 -0500

    Pushing 4.21

---
+++ BiopunchJam3/Intermediate/Build/Win64/BiopunchJam3Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Antoine/Desktop/biopunchteam_03/BiopunchJam3/Intermediate/Build/Win64/BiopunchJam3Editor/Development/Engine/SharedPCH.Engine.h"
---
+++ BiopunchJam3/Source/BiopunchJam3/JamBlueprintFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "JamBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIOPUNCHJAM3_API UJamBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
