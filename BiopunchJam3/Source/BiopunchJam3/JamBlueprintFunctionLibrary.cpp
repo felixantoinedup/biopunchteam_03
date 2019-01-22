609f1314819fe79f546c3077dd988d844ac0558c
antronik7 <antoineberthiaumedutil@gmail.com>
2019-01-22 02:01:07 -0500

    Last version

---
+++ BiopunchJam3/Source/BiopunchJam3/JamBlueprintFunctionLibrary.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "JamBlueprintFunctionLibrary.h"
+
+int UJamBlueprintFunctionLibrary::TestFunction()
+{
+	return 2 + 2;
+}
