d3b84cba270fe99f677283397b9d12ee42ea98fd
Antoine Dutil <antoineberthiaumedutil@gmail.com>
2019-02-14 01:20:10 -0500

    reset

--- BiopunchJam3/Source/BiopunchJam3/JamBlueprintFunctionLibrary.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "JamBlueprintFunctionLibrary.h"
-
-int UJamBlueprintFunctionLibrary::TestFunction()
-{
-	return 2 + 2;
-}
